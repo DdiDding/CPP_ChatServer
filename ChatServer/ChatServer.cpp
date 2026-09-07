49d13a5bfc4547c590d3da279600bc2d79b2f268
yeo myoung Kim <kdjwand@gmail.com>
2026-09-07 17:56:24 +0900

    [S] 간단한 에코 서버 구현
    - Winsock 초기화 및 생성
    - 데이터 수신 후 그대로 송신
    - 연결 해제

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -1,9 +1,137 @@
-﻿// ChatServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <winsock2.h> // winsock.h와는 충돌하므로 주의
+#include <ws2tcpip.h>
+#include <stdio.h>
 
-#include <iostream>
+#define DEFAULT_PORT "27015"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// winsock 구현에 대한 정보가 포함된 구조체
+	WSADATA wsaData;
+
+
+	// Init winsock
+	int iResult;
+	{
+		//WS2_32.dll사용을 시작하기 위해 호출
+		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+		if (iResult != 0) 
+		{
+			printf("WSAStartup failed: %d\n", iResult);
+			return 1;
+		}
+	}
+
+	// 서버에서 사용할 로컬 주소와 포트를 결정
+	struct addrinfo* result = NULL;
+	struct addrinfo* ptr = NULL;
+	struct addrinfo hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET; 
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// 소켓 생성
+	SOCKET ListenSocket = INVALID_SOCKET;
+	{
+		ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if (ListenSocket == INVALID_SOCKET)
+		{
+			printf("socket failed: %ld\n", WSAGetLastError());
+			freeaddrinfo(result);
+			WSACleanup();
+			return 1;
+		}
+	}
+	
+	// 소켓을 현재 로컬 주소에 바인딩
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind failed: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// 소켓 수신 대기
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) 
+	{
+		printf("Listen failed with error: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// 연결 수락
+	SOCKET ClientSocket = INVALID_SOCKET;
+
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		printf("accept failed: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// 데이터 수신 및 송신
+	char recvbuf[512];
+	int iSendResult;
+	int recvbuflen = 512;
+
+	do
+	{
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0)
+		{
+			// 클라이언트에게 그대로 데이터 돌려주기
+			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+			if (iSendResult == SOCKET_ERROR)
+			{
+				printf("send failed: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+		}
+		else if (iResult == 0)
+		{
+			printf("Connection closing...\n");
+		}
+		else
+		{
+			printf("recv failed: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+	} while (iResult > 0);
+
+	// 연결 종료
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
 }
